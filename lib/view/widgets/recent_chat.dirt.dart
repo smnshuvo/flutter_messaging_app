a3fab8cacac159662ec93bd03410fdf364388932
smnshuvo <smnshuvo@gmail.com>
2020-07-15 00:10:15 +0600

    Added recent chats

--- lib/view/widgets/recent_chat.dirt.dart
+++ lib/view/widgets/recent_chat.dirt.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttermessagingapp/model/message_model.dart';
 
 class RecentChats extends StatelessWidget {
   @override
@@ -24,6 +25,35 @@ class RecentChats extends StatelessWidget {
             ],
           ),
         ),
+        Container(
+          height: 120.0,
+          child: ListView.builder(
+            padding: EdgeInsets.only(left: 10.0),
+            scrollDirection: Axis.horizontal,
+            itemCount: favorites.length,
+            itemBuilder: (BuildContext context, int index){
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 35.0,
+                      backgroundImage: AssetImage(favorites[index].imageUrl),
+                    ),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Text(favorites[index].name, style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w500,
+                    ),),
+                  ],
+                ),
+              );
+            },
+          ),
+        )
       ],
     );
   }
